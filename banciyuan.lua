b16562e5265d62650fd44c52338886aa4c09e354
arkiver <arkiver@protonmail.com>
2023-07-11 20:01:16 +0200

    Version 20230711.02. Do not extract URLs from bcyimg items.

--- banciyuan.lua
+++ banciyuan.lua
@@ -447,7 +447,8 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
   end
 
   if allowed(url)
-    and status_code < 300 then
+    and status_code < 300
+    and item_type ~= "bcyimg" then
     html = read_file(file)
     local ssr = get_ssr_data(html)
     if ssr then
